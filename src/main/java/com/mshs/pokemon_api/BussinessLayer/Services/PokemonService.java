fcc7fdac61ffbba68962e650e11e266348fd5acd
susanahdezs02 <susanahdezs02@gmail.com>
2024-08-12 14:03:51 -0600

    Update PokemonService.java

--- src/main/java/com/mshs/pokemon_api/BussinessLayer/Services/PokemonService.java
+++ src/main/java/com/mshs/pokemon_api/BussinessLayer/Services/PokemonService.java
@@ -10,12 +10,10 @@ public class PokemonService {
     private static final String POKEAPI_URL = "https://pokeapi.co/api/v2/pokemon/";
 
     public PokemonDTO processPokemonById(Long id) {
-        // Llama a la API externa para obtener los datos del Pokémon
         RestTemplate restTemplate = new RestTemplate();
         String url = POKEAPI_URL + id;
         PokemonDTO pokemon = restTemplate.getForObject(url, PokemonDTO.class);
-
-        // Procesa y retorna el Pokémon
+        
         return pokemon;
     }
 
