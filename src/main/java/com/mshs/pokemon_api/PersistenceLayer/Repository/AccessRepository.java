f1a41e638f4aa987e730e3982dce25ab17110268
susanahdezs02 <susanahdezs02@gmail.com>
2024-08-12 14:05:46 -0600

    Update AccessRepository.java

--- src/main/java/com/mshs/pokemon_api/PersistenceLayer/Repository/AccessRepository.java
+++ src/main/java/com/mshs/pokemon_api/PersistenceLayer/Repository/AccessRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.mshs.pokemon_api.PersistenceLayer.Entity.PokemonAccess;
 
 
-@Repository // Repositorio de nuestro objeto repositorio
+@Repository 
 public interface AccessRepository extends JpaRepository<PokemonAccess, Long>{
 
 }
